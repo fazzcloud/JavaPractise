2f6ae21eef8c20082d5d2b921200c6c41099a466
fazzcloud <2627828473@qq.com>
2021-12-12 20:22:58 +0800

    12.12字符串连接符与三元运算

---
+++ src/practise/test/operator/Demo12.java
@@ -0,0 +1,15 @@
+package practise.test.operator;
+
+/**
+ * @Author: Fazzcloud
+ * @Date: 2021/12/12 20:09
+ * @Description: 手握日月摘星辰，世间无我这般人
+ */
+public class Demo12 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(""+a+b); //输出的字符串在前，那么a+b不会计算
+        System.out.println(a+b+"");//输出的字符串在后，那么a+b会被计算
+    }
+}
---
+++ src/practise/test/operator/Demo13.java
@@ -0,0 +1,17 @@
+package practise.test.operator;
+
+/**
+ * @Author: Fazzcloud
+ * @Date: 2021/12/12 20:18
+ * @Description: 手握日月摘星辰，世间无我这般人
+ */
+public class Demo13 {
+    public static void main(String[] args) {
+        //x ？ y：z
+        //如果x == true 那么结果为y，否则结果为z
+        int score = 98;
+        String type = score < 60 ? "不及格":"及格";
+        //效果与if相同
+        System.out.println(type);
+    }
+}
